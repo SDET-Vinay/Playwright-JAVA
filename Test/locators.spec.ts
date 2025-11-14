82d1535bc90ce2f089481f2c3634e19602f20327
Vinay Chaudhary <vinay.10845602@ltimindtree.com>
2025-11-14 09:06:34 +0530

    playwright sample tests

---
+++ Test/locators.spec.ts
@@ -0,0 +1,11 @@
+import {test, expect, Locator} from '@playwright/test';
+
+test('locators', async ({page}) => {
+
+    await page.goto("https://www.google.com/");
+
+    const searchbar : Locator = await  page.locator("xpath =//div[@class='SDkEP']").fill("Testesrs")
+
+
+    const searchbutton: Locator = await page.getByRole("button", {name: "button"}).click();
+});
